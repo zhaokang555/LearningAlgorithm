7530a09f5ee83d24b1c484c7555b867633953abe
Zhao Kang <1003994970@qq.com>
2016-02-22 22:04:40 +0800

    quickSort

--- 04-快速排序/usingJS/quicksort.js
+++ 04-快速排序/usingJS/quicksort.js
@@ -1,13 +1,9 @@
-function quicksort(arr, left, right) {
-    // 跳出递归的条件
-    if (left >= right)
-        return;
-
+function quickSort(arr, left, right) {
     // 判断参数个数
     switch (arguments.length) {
         case 1:
-            left = arr[0];
-            right = arr[arr.length - 1];
+            left = 0;
+            right = arr.length - 1;
             break;
         case 3:
             break;
@@ -15,15 +11,19 @@ function quicksort(arr, left, right) {
             throw Error('number of arguments must be 1 or 3');
     }
 
+    // 跳出递归的条件
+    if (left >= right)
+        return;
+
     var i = left,
         j = right,
         temp = arr[i];
     while (i < j) {
         // 以arr[left]为基准
         // 顺序很重要, 要先从右往左找
-        while (arr[j] >= arr[left])
+        while (arr[j] >= arr[left] && i < j)
             j--;
-        while (arr[i] <= arr[left])
+        while (arr[i] <= arr[left] && i < j)
             i++;
 
         // 交换两个数在数组中的位置
@@ -34,11 +34,11 @@ function quicksort(arr, left, right) {
     }
 
     // 最终将基准数归位
-    temp = a[left];
-    a[left] = a[i];
-    a[i] = temp;
+    temp = arr[left];
+    arr[left] = arr[i];
+    arr[i] = temp;
 
-    quicksort(arr, left, i - 1); // 继续处理左边的，这里是一个递归的过程
-    quicksort(arr, i + 1, right); // 继续处理右边的，这里是一个递归的过程
+    quickSort(arr, left, i - 1); // 继续处理左边的，这里是一个递归的过程
+    quickSort(arr, i + 1, right); // 继续处理右边的，这里是一个递归的过程
 }
 
