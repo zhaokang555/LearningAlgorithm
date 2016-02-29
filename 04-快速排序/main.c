bbab1e73783165565a82370396d5715267d1dc9c
Zhao Kang <1003994970@qq.com>
2016-02-29 20:50:01 +0800

    refactor quicksort

--- 04-快速排序/main.c
+++ 04-快速排序/main.c
@@ -1,35 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void quickSort(int a[], int left, int right)
+void swap(int arr[], int i, int j) {
+	int temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
+}
+
+void quickSort(int arr[], int left, int right)
 {
 	// 跳出递归的条件
 	if (left > right) return;
 
 	int i = left;
 	int j = right;
+	int base = left;
 	while (i < j) {
-		// 以a[left]为基准
+		// 以arr[left]为基准
 		// 顺序很重要，要先从右往左找
-		while (a[j] >= a[left] && i < j) j--;
+		while (arr[j] >= arr[base] && i < j) j--;
 		// 再从左往右找
-		while (a[i] <= a[left] && i < j) i++;
+		while (arr[i] <= arr[base] && i < j) i++;
 
+		// 当哨兵i和哨兵j没有相遇时
 		// 交换两个数在数组中的位置
-		if (i < j) { // 当哨兵i和哨兵j没有相遇时
-			int temp = a[i];
-			a[i] = a[j];
-			a[j] = temp;
-		}
+		if (i < j)  
+			swap(arr, i, j);
 	}
 
 	// 最终将基准数归位
-	int temp = a[left]; 
-	a[left] = a[i];
-	a[i] = temp;
+	swap(arr, base, i);
 
-	quickSort(a, left, i - 1); // 继续处理左边的，这里是一个递归的过程
-	quickSort(a, i + 1, right); // 继续处理右边的，这里是一个递归的过程
+	quickSort(arr, left, i - 1); // 继续处理左边的，这里是一个递归的过程
+	quickSort(arr, i + 1, right); // 继续处理右边的，这里是一个递归的过程
 }
 
 int main(void)
@@ -40,17 +43,17 @@ int main(void)
 	scanf("%d", &n);
 
 	// 输入数据
-	int a[n];
+	int arr[n];
 	printf("Input numbers:\n");
 	for (int i = 0; i < n; i++)
-		scanf("%d", &a[i]);
+		scanf("%d", &arr[i]);
 
-	quickSort(a, 0, n - 1); // 快速排序调用
+	quickSort(arr, 0, n - 1); // 快速排序调用
 
 	// 输出排序后的结果
 	printf("The numbers in ascending order are:\n");
 	for (int i = 0; i < n; i++)
-		printf("%d\n", a[i]);
+		printf("%d\n", arr[i]);
 
 	system("pause");
 	return 0;
