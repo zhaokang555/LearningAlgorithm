566ed627b920a2f1e343c687621cb9d22c5dfa0d
Zhao Kang <1003994970@qq.com>
2016-03-13 13:49:53 +0800

    bug fix

--- 12-数组循环左移或右移/数组循环左移或右移.js
+++ 12-数组循环左移或右移/数组循环左移或右移.js
@@ -9,14 +9,16 @@ function reverse(arr, left, right) {
 
 // 数组循环左移m位
 function f1 (arr, m) {
-    reverse(arr, 0, arr.length - 1);
+    rear = arr.length - 1;
     reverse(arr, 0, m - 1);
-    reverse(arr, m, arr.length -1);
+    reverse(arr, m, rear);
+    reverse(arr, 0, rear);
 }
 
 // 数组循环右移m位
 function f2 (arr, m) {
-    reverse(arr, 0, arr.length - 1);
+    rear = arr.length - 1;
+    reverse(arr, 0, rear);
     reverse(arr, 0, m - 1);
-    reverse(arr, m, arr.length -1);
+    reverse(arr, m, rear);
 }
